32b21c0f49b348d3e02395d871dd1f9cba062481
GabrielSgaria <gabrielsgaria2@gmail.com>
2024-01-08 13:28:20 -0300

    responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
   theme: {
+    screens: {
+     
+  
+    },
     extend: {
       boxShadow: {
         '9xl': '-4px 20px 20px 0px black'
