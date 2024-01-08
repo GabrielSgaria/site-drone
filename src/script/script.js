00497638c0c295b9dfa7b9e9f0aad3905594c1ee
GabrielSgaria <gabrielsgaria2@gmail.com>
2024-01-08 14:26:47 -0300

    100%

--- src/script/script.js
+++ src/script/script.js
@@ -14,6 +14,7 @@ function onToggleMenu(e) {
     icon.classList.toggle('right-5')
 }   
 
+
 document.getElementById('scrollToPortfolio').addEventListener('click', function() {
   document.getElementById('portfolio').scrollIntoView({
       behavior: 'smooth',
@@ -41,6 +42,14 @@ document.getElementById('scrollToTop').addEventListener('click', function() {
       block: 'center'
   });
 });
+
+function enviarMensagem(numeroTelefone, mensagemPadrao) {
+
+  var linkWhatsApp = 'https://wa.me/' + numeroTelefone + '?text=' + encodeURIComponent(mensagemPadrao);
+
+  window.open(linkWhatsApp, '_blank');
+}
+
 
 let swiper = new Swiper('.swiper', {
     // Optional parametersa
